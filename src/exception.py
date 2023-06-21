77b4411e7ac803ca850fe19dd67e15ab25e0c11a
Rosanne Odiero <rosanneodiero9@gmail.com>
2023-06-21 04:37:51 +0300

    logexceptionfiles

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys  # Importing the sys module
+from logger import logging  # Importing the logging module from src.logger
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()  # Getting the exception traceback
+    file_name = exc_tb.tb_frame.f_code.co_filename  # Getting the filename where the error occurred
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))  # Creating the error message with the file name, line number, and error
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)  # Generating error message using the error_message_detail function
+    
+    def __str__(self):
+        return self.error_message
+
+if __name__=="__main__": 
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,22 @@
+
+import logging 
+import os
+from datetime import datetime
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" # Creating a log file name based on the current date and time
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE) # Creating the logs path by joining the current working directory, "logs" folder, and the log file name
+os.makedirs(logs_path, exist_ok=True) # Creating the logs path directory if it doesn't exist
+
+# Creating the log file path by joining the logs path and the log file name
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configuring the logging settings
+logging.basicConfig(
+    filename=LOG_FILE_PATH,  # Setting the log file path
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Setting the log message format
+    level=logging.INFO,  # Setting the log level to INFO
+)
+
+if __name__=="__main__": 
+    logging.info("Logging has started")
